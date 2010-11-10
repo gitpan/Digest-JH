2217cfcd3071e78c4a106fd4d9ef3fa43bfa4cb4
gray <gray@cpan.org>
2010-11-10 05:59:56 +0000

    Import of GRAY/Digest-JH-0.02 from CPAN.
    gitpan-cpan-distribution: Digest-JH
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         GRAY/Digest-JH-0.02.tar.gz
    gitpan-cpan-author:       GRAY
    gitpan-cpan-maturity:     released

--- lib/Digest/JH.pm
+++ lib/Digest/JH.pm
@@ -6,7 +6,7 @@ use parent qw(Exporter Digest::base);
 
 use MIME::Base64 ();
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 $VERSION = eval $VERSION;
 
 eval {
@@ -50,6 +50,14 @@ sub jh_512_base64 {
     my $b64 = MIME::Base64::encode(jh_512(@_), '');
     $b64 =~ s/=+$//g;
     return $b64;
+}
+
+sub add_bits {
+    my ($self, $data, $bits) = @_;
+    if (2 == @_) {
+        return $self->_add_bits(pack('B*', $data), length $data);
+    }
+    return $self->_add_bits($data, $bits);
 }
 
 
